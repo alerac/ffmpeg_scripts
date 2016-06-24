e5a5159e0341ab253b13e8f18125955f8fbdfdb9
alerac <alexandre.racine1@gmail.com>
2016-06-24 21:43:26 +0200

    improved user interaction with os conditions

--- audio.sh
+++ audio.sh
@@ -6,10 +6,29 @@
 	echo "Choose your OS (eg: linux or osx) without capital letters, same as example, and press ENTER :"
 	read os
 
+	if [ "$os" == "linux" ]
+	then
+	
 # Choose which audio device you want to record writing the number (eg: 1 or 0 or other number) when asked for. The script will adapt the command accorded to your system.
 
-	echo "Choose the audio device you want to record (eg: 1 or 0 or other number) and press ENTER. You can look which number it is according to your system writing 	      [arecord -l] in your shell for Linux or [ffmpeg -f avfoundation -list_devices true -i ""] for osx."
-	read audiodevice
+	echo "Choose the audio device you want to record (eg: 1 or 0 or other number) and press ENTER. You can look which number it is according to your system writing 
+[arecord -l] in your shell :" [ffmpeg -f avfoundation -list_devices true -i \"\"]
+	read lindevice
+	
+	elif [ "$os" == "osx" ]
+	then
+
+	echo "Choose the audio device you want to record (eg: 1 or 0 or other number) and press ENTER. You can look which number it is according to your system writing
+[ffmpeg -f avfoundation -list_devices true -i \"\"] :"
+
+	read osxdevice
+
+	else
+	
+	echo "Your system is neither linux nor osx so this program won't work for now. Please try it with one of these os"
+	
+	exit 1
+	fi
 
 # Choose the path where you want to record audio writing the path when asked for.
 
@@ -25,12 +44,12 @@
 
 	if [ "$os" == "linux" ] && [ -n $path ] && [ -n $name ];
 	then
-	ffmpeg -f alsa -i hw:$audiodevice -c:a aac -qp 0 -preset ultrafast $path/$name.aac
+	ffmpeg -f alsa -i hw:$lindevice -c:a aac -qp 0 -preset ultrafast $path/$name.aac
 	echo "audio stream successfully recorded"
 
 	elif [ "$os" == "osx" ] && [ -n $path ] && [ -n $name ];
 	then
-	ffmpeg -f avfoundation -i ":$audiodevice" -c:a aac -qp 0 -preset ultrafast $path/$name.aac
+	ffmpeg -f avfoundation -i ":$osxdevice" -c:a aac -qp 0 -preset ultrafast $path/$name.aac
 	echo "audio stream successfully recorded"
 	
 	fi
--- video.sh
+++ video.sh
@@ -5,28 +5,30 @@
 	echo "Choose your OS (eg: linux or osx) without capital letters, same as example, and press ENTER :"
 	read os
 
-	if [ "$os" != "linux" ] && ["$os" != "osx" ];
-	then
-	echo "Too bad ! It can not work with your system. Please enter linux or osx to run the script"
-	
-	exit 0
-
-	elif [ "$os" == "linux" ];
-	then
-
 # for Linux, look for your screen resolution (eg: 1920x1080) and enter it when asked for.
 
+	if [ "$os" == "linux" ]
+	then
 	echo "Enter your screen resolution as this example (eg: 1920x1080) and press ENTER :"
 
 	read reslinux
 
-# For osx, type [ffmpeg -f avfoundation -list_devices true -i ""] if you need to know the number related to your screen device. Then type the number related to your screen device when asked for.	
-	
-	else
+# For osx, type [ffmpeg -f avfoundation -list_devices true -i ""] if you need to know the number related to your screen device. Then type the number related to your screen 
+# device when asked for.
 
-	echo "Choose the video device you want to record (eg: 1 or 0 or other number) and press ENTER. You can look which number it is according to your system writing 	      [ffmpeg -f avfoundation -list_devices true -i ""] in your shell."
+	elif [ "$os" == "osx" ]
+	then
+
+	echo "Choose the video device you want to record (eg: 1 or 0 or other number) and press ENTER. You can look which number it is according to your system writing 
+[ffmpeg -f avfoundation -list_devices true -i \"\"] in your shell. :"
 
 	read resosx
+
+	else
+
+	echo "Your system is neither linux nor osx so this program won't work for now. Please try it with one of these os"
+	exit 1
+
 	fi
 
 # Choose the path where you want to record video writing the path when asked for. The path should be the same as the audio file previously recorded for future works.
